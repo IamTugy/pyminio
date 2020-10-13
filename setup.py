47b48a1ce46b8863227ca140a049f19b26abe174
mmm1513 <tugmica@gmail.com>
2020-10-13 22:59:08 +0300

    changed python versions

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
