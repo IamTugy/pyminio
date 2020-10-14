504ad8e779cecbbe776f2a02150624255ebe0c1a
mmm1513 <tugmica@gmail.com>
2020-10-14 19:29:55 +0300

    changed version to 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyminio",
-    version="0.2.0",
+    version="0.2.1",
     author="Michael Tugendhaft",
     author_email="tugmica@gmail.com",
     description="Python client for Minio",
